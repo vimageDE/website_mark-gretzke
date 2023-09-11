6638c2c8e0ce54e670f0c47a261530b0ba8c38d8
vimageDE <m.wierzimok@vimage.de>
2023-09-11 12:32:36 +0200

    Updated Software Selection

--- components/Section_Software.js
+++ components/Section_Software.js
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { motion, useInView } from 'framer-motion';
 
 const containerVariants = {
@@ -29,22 +29,114 @@ const itemVariants = {
   },
 };
 
-const Software = ({ software, className }) => {
+const Software = ({ software, onSoftwareSelected, onBackClicked, isSelected, somethingSelected }) => {
   const symbol = '/Software_' + software.replace(' ', '-') + '_128.png';
+  const [selectedScale, setSelectedScale] = useState(1.5);
+
+  const softwareRef = useRef(null);
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [opacity, setOpacity] = useState(1);
+
+  // This effect runs when isSelected changes
+  useEffect(() => {
+    console.log('Test Use Effect');
+
+    if (isSelected && softwareRef.current) {
+      const parent = softwareRef.current.closest('.parent-container');
+      if (parent) {
+        const parentRect = parent.getBoundingClientRect();
+        const softwareRect = softwareRef.current.getBoundingClientRect();
+
+        const xCenter = (parentRect.width - softwareRect.width) / 2;
+        const yCenter = (parentRect.height - softwareRect.height) / 3;
+
+        // Correct the offset to place the object exactly at the center
+        const offsetX = xCenter - (softwareRect.left - parentRect.left);
+        const offsetY = yCenter - (softwareRect.top - parentRect.top);
+
+        setX(offsetX);
+        setY(offsetY);
+
+        setOpacity(1);
+        setSelectedScale(1.5);
+      }
+    } else {
+      setX(0);
+      setY(0);
+      setSelectedScale(1);
+
+      if (somethingSelected) {
+        setOpacity(0);
+      } else {
+        setOpacity(1);
+      }
+    }
+  }, [isSelected, somethingSelected]);
+
+  const variants = {
+    initial: { ...itemVariants.initial },
+    whileInView: { ...itemVariants.whileInView },
+    animate: { scale: selectedScale, x, y, opacity },
+  };
 
   return (
-    <motion.div className={className} variants={itemVariants} whileHover={{ scale: 1.5 }}>
-      <div className="w-16 h-16 bg-contain mx-auto mb-1" style={{ backgroundImage: `url(${symbol})` }}></div>
-      <div className="text-xs">{software}</div>
+    <motion.div
+      ref={softwareRef}
+      initial="initial"
+      animate={{ scale: selectedScale, x, y, opacity }}
+      variants={itemVariants}
+      onHoverStart={() => {
+        if (!somethingSelected) setSelectedScale(1.5);
+      }}
+      onHoverEnd={() => {
+        if (!somethingSelected) setSelectedScale(1);
+      }}
+    >
+      <div
+        onClick={() => onSoftwareSelected(software)}
+        className={`w-16 h-16 bg-contain mx-auto mb-1`}
+        style={{ backgroundImage: `url(${symbol})` }}
+      ></div>
+      <div onClick={() => onSoftwareSelected(software)} className="text-xs">
+        {software}
+      </div>
+      {isSelected ? (
+        <motion.div className="absolute" initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+          <p>This is a test for a text</p>
+          <motion.div
+            whileHover={{ scale: 1.5 }}
+            className="bg-gold py-1 px-4 rounded cursor-pointer mt-8"
+            onClick={() => {
+              onBackClicked();
+            }}
+          >
+            <h2 className="text-black text-xl">BACK</h2>
+          </motion.div>
+        </motion.div>
+      ) : (
+        <></>
+      )}
     </motion.div>
   );
 };
 
 export default function SoftwareSection() {
+  const [selectedSoftware, setSelectedSoftware] = useState(null);
+
   const isinView = useInView({
     margin: '0px -500px -50px 0px',
   });
   const bgImage = '/background-software1.png';
+
+  const onSoftwareSelected = (software) => {
+    setSelectedSoftware(software);
+  };
+  const onBackClicked = () => {
+    console.log('Test Back clicked');
+    setSelectedSoftware(null);
+    console.log('Selected Software is:', selectedSoftware);
+  };
 
   return (
     <div className="h-screen flex relative overflow-hidden">
@@ -68,14 +160,50 @@ export default function SoftwareSection() {
             variants={containerVariants}
             initial="initial"
             whileInView="whileInView"
-            className="grid grid-cols-3 gap-16 text-center mt-8"
+            className="flex gap-16 w-[350px] flex-wrap justify-center text-center mt-8 relative parent-container"
           >
-            <Software software={'C4D'} className="" />
-            <Software software={'After Effects'} className="" />
-            <Software software={'Octane'} className="" />
-            <Software software={'Unity'} className="" />
-            <Software software={'Solidity'} className="" />
-            <Software software={'Webdev'} className="" />
+            <Software
+              software={'C4D'}
+              onSoftwareSelected={onSoftwareSelected}
+              onBackClicked={onBackClicked}
+              isSelected={selectedSoftware === 'C4D'}
+              somethingSelected={selectedSoftware !== null}
+            />
+            <Software
+              software={'After Effects'}
+              onSoftwareSelected={onSoftwareSelected}
+              onBackClicked={onBackClicked}
+              isSelected={selectedSoftware === 'After Effects'}
+              somethingSelected={selectedSoftware !== null}
+            />
+            <Software
+              software={'Octane'}
+              onSoftwareSelected={onSoftwareSelected}
+              onBackClicked={onBackClicked}
+              isSelected={selectedSoftware === 'Octane'}
+              somethingSelected={selectedSoftware !== null}
+            />
+            <Software
+              software={'Unity'}
+              onSoftwareSelected={onSoftwareSelected}
+              onBackClicked={onBackClicked}
+              isSelected={selectedSoftware === 'Unity'}
+              somethingSelected={selectedSoftware !== null}
+            />
+            <Software
+              software={'Solidity'}
+              onSoftwareSelected={onSoftwareSelected}
+              onBackClicked={onBackClicked}
+              isSelected={selectedSoftware === 'Solidity'}
+              somethingSelected={selectedSoftware !== null}
+            />
+            <Software
+              software={'Webdev'}
+              onSoftwareSelected={onSoftwareSelected}
+              onBackClicked={onBackClicked}
+              isSelected={selectedSoftware === 'Webdev'}
+              somethingSelected={selectedSoftware !== null}
+            />
           </motion.div>
         </div>
       </div>
